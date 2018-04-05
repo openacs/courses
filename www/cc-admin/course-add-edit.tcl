9ff4da65230fd71146d7db2ecbc868d950277348
Hector Romojaro <hector.romojaro@gmail.com>
2018-04-05 14:44:09 +0000

    Fix typos

--- www/cc-admin/course-add-edit.tcl
+++ www/cc-admin/course-add-edit.tcl
@@ -40,7 +40,7 @@ db_foreach assessment { } {
     }
 }
 
-# Get a list of all the attributes asociated to course_catalog
+# Get a list of all the attributes associated to course_catalog
 set attribute_list [package_object_attribute_list -start_with course_catalog course_catalog]
 set elements ""
 
